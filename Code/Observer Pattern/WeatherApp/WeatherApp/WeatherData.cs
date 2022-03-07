d4564f82f4b53640eb3acbc2fd1442935dab497b
Eddy Salas <eddysalas1@icloud.com>
2022-03-06 17:33:08 -0800

    Observer method notes and exercises

---
+++ Code/Observer Pattern/WeatherApp/WeatherApp/WeatherData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public class WeatherData
+    {
+        // Instance variable declarations here:
+
+        public void MeasurementsChanged()
+        {
+            double temp = GetTemperature();
+            double humidity = GetHumidity();
+            double pressure = GetPressure();
+
+            currentConditionsDisplay.update();
+            statisticsDisplayUpdate.update();
+            forecastDisplay.update();
+
+
+        }
+    }
+}
