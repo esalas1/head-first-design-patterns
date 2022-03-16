71a6461f1cd9679a89d6f01a8bd4ad648173373e
Eddy Salas <eddysalas1@icloud.com>
2022-03-15 21:36:17 -0700

    Singeleton notes and code

---
+++ Code/Singleton Pattern/ChocolateFactoryApp/ChocolateFactoryApp/ChocolateBoiler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChocolateFactoryApp
+{
+    public class ChocolateBoiler
+    {
+        private Boolean empty;
+        private Boolean boiled;
+
+        private static ChocolateBoiler _chocolateBoiler;
+
+        public static ChocolateBoiler GetInstance()
+        {
+            if (_chocolateBoiler == null)
+            {
+                _chocolateBoiler = new ChocolateBoiler(); 
+            }
+
+            return _chocolateBoiler;
+
+        }
+
+        private ChocolateBoiler()
+        {
+            empty = true;
+            boiled = false;
+        }
+    }
+}
